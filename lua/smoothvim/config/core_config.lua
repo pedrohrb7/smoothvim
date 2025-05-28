e4ba88e1701e397e6de94d01405920da312d3846
Pedro <pedrohrb77@gmail.com>
2025-05-27 23:21:07 -0300

    feat(config): implement core and keymaps

--- lua/smoothvim/config/core_config.lua
+++ lua/smoothvim/config/core_config.lua
@@ -1,3 +1,49 @@
 -- Here is the core config, nvim/vim config
 -- This config does not relates to any plugin config
 vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartindent = true
+
+opt.cursorline = true
+opt.cursorcolumn = true
+opt.fileencoding = "utf-8"
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- Cursor config
+opt.guicursor = ""
+  .. "n-v-c-sm:ver25-lCursor/lCursor," -- Normal
+  .. "i-ci-ve:ver25-lCursor/lCursor," -- Insert
+  .. "r-cr-o:hor20-vCursor/vCursor," -- Replace
+  .. "a:blinkon100-blinkoff100" -- Blink
+
+-- turn off swapfile
+opt.swapfile = false
--- lua/smoothvim/config/core_keymaps.lua
+++ lua/smoothvim/config/core_keymaps.lua
@@ -1,3 +1,49 @@
 -- Same as core_config but keymaps
 -- Keymaps that does not exec any plugin
-return {}
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+
+local keymap = vim.keymaplocal
+local opts = { noremap = true, silent = true }
+
+keymap.set("i", "jk", "<ESC>", opts, { desc = "Exit insert mode with jk" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", opts, { desc = "Clear search highlights" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", opts, { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", opts, { desc = "Decrement number" }) -- decrement
+
+keymap.set("n", "<C-s>", "<cmd>:update<CR>", opts, { desc = "Update file changes" })
+keymap.set("n", "<C-q>", "<cmd>:q<CR>", opts, { desc = "Quit nvim" })
+
+-- Visual Block --
+-- Move text up and down
+keymap.set("n", "<A-j>", ":m .+1<CR>==", opts, { desc = "move line up (normal mode)" }) -- move line up(n)
+keymap.set("n", "<A-k>", ":m .-2<CR>==", opts, { desc = "move line down(normal mode)" }) -- move line down(n)
+keymap.set("v", "<A-k>", ":move '<-2<CR>gv-gv", opts, { desc = "move text block up" })
+keymap.set("v", "<A-j>", ":move '>+1<CR>gv-gv", opts, { desc = "move text block down" })
+
+-- Resize window using <ctrl> arrow keys
+keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", opts, { desc = "Increase Window Height" })
+keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", opts, { desc = "Decrease Window Height" })
+keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", opts, { desc = "Decrease Window Width" })
+keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", opts, { desc = "Increase Window Width" })
+
+-- Stay in indent mode
+keymap.set("v", "<", "<gv", opts, { desc = "Indent mode on back" })
+keymap.set("v", ">", ">gv", opts, { desc = "Indent mode on indenting" })
+
+-- Keep last yanked when pasting
+keymap.set("v", "p", '"_dP', opts)
+
+-- Autocommands
+vim.api.nvim_create_augroup("custom_buffer", { clear = true })
+
+-- highlight yanks
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = "custom_buffer",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200 })
+  end,
+})
