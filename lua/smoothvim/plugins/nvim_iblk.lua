7dbc3fba71875f1eef4ead02e21a755a02006cae
Pedro <pedrohrb77@gmail.com>
2025-05-29 00:30:20 -0300

    chore: change iblkl

--- lua/smoothvim/plugins/nvim_iblk.lua
+++ lua/smoothvim/plugins/nvim_iblk.lua
@@ -4,7 +4,6 @@ return {
   opts = {},
   config = function()
     local ibl = require("ibl")
-    local vm = vim
     local highlight = {
       "RainbowRed",
       "RainbowYellow",
@@ -19,17 +18,17 @@ return {
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vm.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vm.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vm.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vm.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vm.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vm.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vm.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-      vm.api.nvim_set_hl(0, "RainbowLavender", { fg = "#45707a" })
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "RainbowLavender", { fg = "#45707a" })
     end)
 
-    vm.g.rainbow_delimiters = { highlight = highlight }
+    vim.g.rainbow_delimiters = { highlight = highlight }
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     ibl.setup({
       debounce = 100,
