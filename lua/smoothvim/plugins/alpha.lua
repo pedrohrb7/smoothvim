b3612dab587f0fa9dae638de04aad20601ef45ef
Pedro <pedrohrb77@gmail.com>
2025-08-16 12:32:43 -0300

    chore: add icons to alpha

--- lua/smoothvim/plugins/alpha.lua
+++ lua/smoothvim/plugins/alpha.lua
@@ -107,9 +107,9 @@ return {
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
     }
 
     dashboard.leader = "\\"
