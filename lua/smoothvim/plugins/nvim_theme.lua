665f690231cdc1a72981e0c8e8fdf4640759b82f
Pedro <pedrohrb77@gmail.com>
2026-01-22 18:31:42 -0300

    chore: update theme list

--- lua/smoothvim/plugins/nvim_theme.lua
+++ lua/smoothvim/plugins/nvim_theme.lua
@@ -7,14 +7,8 @@ return {
       -- Get all available colorschemes
       local colorschemes = vim.fn.getcompletion("", "color")
 
-      local filtered_themes = {}
-      for _, theme in ipairs(colorschemes) do
-        -- Check for common light theme name patterns
-        table.insert(filtered_themes, theme)
-      end
-
       require("themery").setup({
-        themes = filtered_themes,
+        themes = colorschemes,
         livePreview = true,
       })
     end,
