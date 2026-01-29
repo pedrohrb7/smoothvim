84b9cc0a5694f8a92afa5b50e9ecee7ba5eb554d
Pedro Borges <pedrohrb77@gmail.com>
2026-01-29 15:33:55 -0300

    update eslint config / tailwind css config

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require("smoothvim.config.options")
 require("smoothvim.config.autocmds")
 require("smoothvim.config.keymaps")
 require("smoothvim.config.lsp")
@@ -5,60 +6,6 @@ require("smoothvim.plugins")
 
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
-
-local opt = vim.opt
-
-opt.relativenumber = true
-opt.number = true
-
--- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
-
-opt.wrap = false
-
--- search settings
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-opt.smartindent = true
-
-opt.cursorline = true
-opt.cursorcolumn = true
-opt.fileencoding = "utf-8"
-
-opt.termguicolors = true
-opt.signcolumn = "yes"
-
--- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
-
--- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
-
--- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
-
-vim.g.markdown_recommended_style = 0
-
-vim.filetype.add({
-  extension = {
-    env = "dotenv",
-  },
-  filename = {
-    [".env"] = "dotenv",
-    ["env"] = "dotenv",
-  },
-  pattern = {
-    ["[jt]sconfig.*.json"] = "jsonc",
-    ["%.env%.[%w_.-]+"] = "dotenv",
-  },
-})
-
-vim.g.autoformat = true
-vim.g.trouble_lualine = true
 
 vim.pack.add({
   -- theme packages
@@ -80,6 +27,8 @@ require("mason-tool-installer").setup({
     "lua_ls",
     "ts_ls",
     "eslint_d",
+    "tailwindcss-language-server",
+    "prettierd",
   },
 })
 
---
+++ lua/smoothvim/config/options.lua
@@ -0,0 +1,53 @@
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartindent = true
+
+opt.cursorline = true
+opt.cursorcolumn = true
+opt.fileencoding = "utf-8"
+
+opt.termguicolors = true
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+vim.g.markdown_recommended_style = 0
+
+vim.filetype.add({
+  extension = {
+    env = "dotenv",
+  },
+  filename = {
+    [".env"] = "dotenv",
+    ["env"] = "dotenv",
+  },
+  pattern = {
+    ["[jt]sconfig.*.json"] = "jsonc",
+    ["%.env%.[%w_.-]+"] = "dotenv",
+  },
+})
+
+vim.g.autoformat = true
+vim.g.trouble_lualine = true
